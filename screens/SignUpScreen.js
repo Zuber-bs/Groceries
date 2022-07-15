87a81a39349931e97c01c573c7b07fb82fab1a48
Pranav Shishodia <pranavshishodia1105@gmail.com>
2022-07-14 20:05:33 -0500

    Error

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from 'react-native';
 import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 import { useState } from 'react';
 
@@ -6,10 +6,23 @@ export default function LoginScreen() {
 
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const auth = getAuth();
 
     return (
-        <View>
-            <Text>Hi</Text>
+        <View style={styles.container}>
+            <TextInput placeholder='E-mail'/>
+            <TextInput placeholder='Password' secureTextEntry={true}/>
+            <TouchableOpacity>
+                Sign Up
+            </TouchableOpacity>
         </View>
     );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+    }
+});
